b3de15be22a5427210e5b34ceefd581b3e8ddeff
Job Jimmy Lebelwa Danmou <leb@neusta.de>
2022-03-14 19:40:02 +0100

    new chnages

--- ParticleTrackingSystem/restart.py
+++ ParticleTrackingSystem/restart.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
-#from Cython.Utility.MemoryView import memoryview
+# from Cython.Utility.MemoryView import memoryview
 from pandas import DataFrame, Series  # for convenience
 
 import pims
@@ -51,40 +51,46 @@ if __name__ == '__main__':
     print('Hi PyCharm')
     frames = gray(convert_into_image_sequence('./BW/BW-Isil-video4.avi'))
     print('----------')
-    frames
-    print('Type of Frames ' + str(type(frames)))
-    print('----------')
-    print(frames[0])
-    print('Type of Frames[0] ' + str(type(frames[0])))
-    print('----------')
     plt.imshow(frames[0])
     plt.show()
-    print('------##----')
+
+    print('------##------')
+
     # Localise les taches de types Gaussiens d'une taille approxi. dans une image.
-    #Ici dans l'image 0
+    # Ici dans l'image 0
     print('Data of 1st image')
     f = tp.locate(frames[0], 5, False)
     print('Type of f ' + str(type(f)))
     print(f.head())
+    print(len(f))
     # Localise les taches de types Gaussiens d'une taille approxi. dans une image.
-    #Ici dans l'image 1
+    # Ici dans l'image 1
     print('Data of 2nd image')
     f = tp.locate(frames[1], 5, False)
+    print(f.head())
+    print(len(f))
+
+    # Number of particles per image
+    print("Loop is starting...")
+    cnt = 0
+    for i in frames[:98]:
+        f = tp.locate(frames[cnt], 5, False)
+        print(len(f))
+        cnt += 1
+    print("Loop is ending...")
 
     # Returns information of the first 5 founded particles(y,x,mass,size,ecc,signal,raw_mass,ep,frame)
-    print(f.head())
+    # print(f.head())
     # Mark identified features with white circles.
     tp.annotate(f, frames[0])
-    print(f.at[0,'mass'])
+    print(f.at[0, 'mass'])
     print(f.at[99, 'mass'])
 
-    x_werte = []
-    cr = 0
-    for image in frames[:]:
-        print('Image type: '+ str(type(image)))
-        print(cr)
-        cr += 1
+    # f = tp.batch(frames[:30], 5, minmass=30, invert=True)
+    # print("Batch function returns a: " + str(type(f)))
 
+    w, h = 9, 31
+    matrix = [[0 for x in range(w)] for y in range(h)]
 
-    
+    # print(str(f))
     print('Bye PyCharm')
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # BA-ParticleTrackingSystem
+
+ffmpeg muss als Paket in installiert werden.
+scikit-image(image-reader) muss auch als Paket installiert werden
